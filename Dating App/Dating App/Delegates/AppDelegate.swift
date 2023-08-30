a4413a622b06cbf135673390f0159e10990385d4
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-08-31 00:06:33 +0100

    updated

--- Dating App/Dating App/Delegates/AppDelegate.swift
+++ Dating App/Dating App/Delegates/AppDelegate.swift
@@ -6,10 +6,17 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
+    }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
