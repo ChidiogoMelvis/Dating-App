cd48e181c85faf66129afc423c6016154b06ee80
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-09-11 22:18:00 +0100

    updated

---
+++ Dating App/Dating App/Extension+ViewController/Extensions+HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  Extensions+HomeViewController.swift
+//  Dating App
+//
+//  Created by Mac on 11/09/2023.
+//
+
+import Foundation
+import UIKit
+import MessageKit
+
+extension HomeViewController {
+   
+//    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessageKit.MessagesCollectionView) -> MessageKit.MessageType {
+//        let chatMessages: [MessageType] = [
+//            ]
+//            let message = chatMessages[indexPath.section]
+//        return message
+//    }
+//
+//    func numberOfSections(in messagesCollectionView: MessageKit.MessagesCollectionView) -> Int {
+//        return 1
+//    }
+//
+//
+}
